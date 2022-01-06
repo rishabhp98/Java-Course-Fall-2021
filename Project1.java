f0e2286402b11f8c952e5d77abf248963336284a
Rishabh Pandey <96506739+rishabhp98@users.noreply.github.com>
2022-01-06 16:00:58 -0600

    Add files via upload

---
+++ Project1.java
@@ -0,0 +1,18 @@
+
+public class Project1 
+
+{
+
+/**
+ * This is my first Java program.
+ * @author Rishabh Pandey
+ * @version 1.0
+ */
+	
+	public static void main(String[]args)
+	{
+		
+		System.out.println("What do you call 8 rabbits?" + "A rabbyte!");
+	}
+	
+}
